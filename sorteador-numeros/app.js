b9da2cbc6e5ff9d2649749804e0ea9f5f59f6b33
luannJS <luanbruizjs@gmail.com>
2024-07-18 15:33:13 -0300

    Add files via upload

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,36 @@
+function sortear() {
+let quantidade = parseInt(document.getElementById('quantidade').value);
+let de = parseInt(document.getElementById('de').value);
+let ate = parseInt(document.getElementById('ate').value);
+
+let sorteados = [];
+let numero;
+
+for (let i = 0; i < quantidade; i++) {
+numero = obterNumeroAleatorio(de, ate);
+
+while (sorteados.includes(numero)) {
+numero = obterNumeroAleatorio(de, ate);
+
+}
+sorteados.push(numero);
+
+}
+let resultado = document.getElementById('resultado');
+resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+
+
+}
+
+function obterNumeroAleatorio(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    alterarStatusBotao();
+}
